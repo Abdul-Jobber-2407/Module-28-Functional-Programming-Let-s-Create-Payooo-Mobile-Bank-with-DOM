45ba3d9f8ac6e4a09868fb6865723348629c0880
Abdul-Jobber-2407 <arjobbar2019@gmail.com>
2024-11-25 12:48:48 +0600

    added to a final push

---
+++ js/cashOut.js
@@ -0,0 +1,15 @@
+document.getElementById('btn-cash-out')
+    .addEventListener('click', function(event){
+        event.preventDefault();
+        const cashOut = getInputFieldValueById ('input-cash-out');
+        const PinNumber = getInputFieldValueById('input-cash-out-pin');
+        // console.log('inside the click handler', cashOut, PinNumber);
+        if(PinNumber=== 1234){
+            const balance = getTextFieldValueById('account-balance');
+            const newBalance = balance - cashOut;
+            document.getElementById('account-balance').innerText = newBalance;
+        }
+        else{
+            alert('No money for you......DGM')
+        }
+    })
